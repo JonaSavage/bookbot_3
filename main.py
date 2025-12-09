19a0e9d2acfd8bd7719e2a3124c31aaa60ca182f
JonaSavage <jonathan.stickle@gmail.com>
2025-12-09 14:45:53 -0800

    Report is properly displayed

--- main.py
+++ main.py
@@ -5,10 +5,17 @@ def main():
     text = get_book_text(book)
     word_count = get_num_words(text)
     char_count = get_char_count(text)
+    char_count_sorted = sorted_char_count(char_count)
     
     #print(text)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book}...")
+    print("----------- Word Count ----------")
     print(f"There are {word_count} total words in this text.")
-    print(char_count)
+    print("--------- Character Count -------")
+    for i in char_count_sorted:
+        print(f"{i["char"]}: {i["count"]}")
+    #print(char_count)
 
 
 def get_book_text(filepath):
--- stats.py
+++ stats.py
@@ -14,4 +14,26 @@ def get_char_count(book_text):
             char_dic[c] += 1
             
     return char_dic
+
+
+
+def sort_on(char_dict):
+    return char_dict["count"]
+    
+    
+def sorted_char_count(char_dict):
+    list_char_dicts = []
+    for i in char_dict:
+        if not i.isalpha():
+            continue
+        
+        temp = {
+            "char": i,
+            "count": char_dict[i],
+            
+        }
+        list_char_dicts.append(temp)
+    
+    list_char_dicts.sort(reverse=True, key=sort_on)
+    return list_char_dicts
         
